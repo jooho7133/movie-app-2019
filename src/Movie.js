240571902189d3a2902f3ec00875d96e447e66bc
jooho7133 <jooho7133@gmail.com>
2021-08-17 19:39:35 +0900

    #4.1 Rendering the Movies

---
+++ src/Movie.js
@@ -0,0 +1,16 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+function Movie(props) {
+  return <h4>{props.title}</h4>;
+}
+
+Movie.propTypes = {
+  id: PropTypes.number.isRequired,
+  year: PropTypes.number.isRequired,
+  title: PropTypes.string.isRequired,
+  summary: PropTypes.string.isRequired,
+  poster: PropTypes.string.isRequired,
+};
+
+export default Movie;
